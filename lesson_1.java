f543704734bac11a02fe3adc9df8b666f42992c6
Alexei Yurikov <roboshadow@mail.ru>
2022-09-11 18:55:57 +0300

    add task2 solution

--- lesson_1.java
+++ lesson_1.java
@@ -5,9 +5,18 @@ import java.util.Arrays;
 public class lesson_1 {
     public static void main(String[] args) {
         //    Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+
         System.out.println(test1(2, -3));
         System.out.println(Arrays.toString(test2(-3)));
         System.out.println(test3("asd4"));
+
+//        Реализуйте метод, принимающий в качестве аргументов два целочисленных массива, и возвращающий новый массив,
+//        каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке. Если длины
+//        массивов не равны, необходимо как-то оповестить пользователя.
+
+        int[] arr1 = {5, 6, 4};
+        int[] arr2 = {3, 2, 2, 7};
+        System.out.println(Arrays.toString(arraysDiff(arr1, arr2)));
     }
 
     public static int test1(int x, int y) {
@@ -29,6 +38,18 @@ public class lesson_1 {
             throw new StringIndexOutOfBoundsException("String length should be more then 4");
         }
         return arg.charAt(5);
+    }
+
+    public static int[] arraysDiff(int[] arr1, int[] arr2) {
+        int[] resultArr = new int[arr1.length];
+        int sum = 0;
+        if (arr1.length != arr2.length) {
+            throw new RuntimeException("Arrays length should be equal!");
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            resultArr[i] = arr1[i] - arr2[i];
+        }
+        return resultArr;
     }
 
 }
